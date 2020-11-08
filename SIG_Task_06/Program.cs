0d5ada3a50ff2ac210561235df4571744fb68227
ILYA-76 <74150630+ILYA-76@users.noreply.github.com>
2020-11-09 00:23:28 +0300

    Add files via upload

---
+++ SIG_Task_06/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SII_Task_06
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            string[][] fio = new string[3][]; 
+            fio[0] = new string[100];//Фамилия
+            fio[1] = new string[100];//Имена
+            fio[2] = new string[100];//Отчество
+
+
+            string[] dolzh = new string[100];// Должность
+            int i = 0;//Количество записей
+            bool isNeeded = true; // Проверка для выхода из массива
+
+
+            do
+            {
+
+
+                Console.Write("Отдел кадров: " +
+                           "\n  1. добавить досье " +
+                           "\n  2. вывести все досье" +
+                           "\n  3. удалить досье" +
+                           "\n  4. поиск по фамилии" +
+                           "\n  5. выход из программы \n \n");
+                Console.Write("Выберите пункт меню: ");
+
+
+                int function;
+                try { function = Convert.ToInt32(Console.ReadLine()); } 
+                catch { function = 0; }
+                Console.Clear();
+
+
+                switch (function)
+                {
+
+
+                    case 1:
+
+
+                        Console.Write("Введите Фамилию: ");
+                        fio[0][i] = Console.ReadLine();
+                        Console.Write("Введите Имя: ");
+                        fio[1][i] = Console.ReadLine();
+                        Console.Write("Введите Отчество: ");
+                        fio[2][i] = Console.ReadLine();
+
+                        Console.Write("Введите должность: "); // Вводим данные о человеке
+                        dolzh[i] = Console.ReadLine();
+                        i++; // Увеличиваем число записей на 1
+                        Console.Write("Данные занесены усспешно. Нажмите любую клавишу...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+
+                    case 2:
+
+
+                        if (i != 0)
+                        {
+
+                            Console.Clear();
+                            Console.WriteLine("Вывод всех досье: \n");
+                            for (int j = 0; j <= i - 1; j++)
+                            {
+                                Console.WriteLine($"{j}. {fio[0][j]} {fio[1][j]} {fio[2][j]} - {dolzh[j]}"); // Выводим все данные
+                            }
+                            Console.Write("\n Нажмите любую клавишу...");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                        }
+                        else
+                        {
+
+                            Console.Write("Не найдено ни одной записи. Нажмите любую кнопку для продолжения!");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                        }
+                        break;
+
+
+                    case 3:
+
+
+                        Console.Write("Введите номер досье для удаления: ");
+                        int choice;
+                    again:
+                        try { choice = Convert.ToInt32(Console.ReadLine()); }
+                        catch { goto again; }
+
+                        bool isFounded = false; 
+                        if (choice <= i && choice > 0) 
+                        {
+
+                            for (int j = 0; j <= i; j++)
+                            {
+
+                                if (j == choice || isFounded)
+                                {
+
+                                    if (j != i - 1) 
+                                    {
+
+                                        fio[0][j] = fio[0][j + 1];
+                                        fio[1][j] = fio[1][j + 1];
+                                        fio[2][j] = fio[2][j + 1];
+                                        dolzh[j] = dolzh[j + 1];
+                                        isFounded = true;
+
+                                    }
+                                    else
+                                    {
+
+                                        fio[0][j] = "";
+                                        fio[1][j] = "";
+                                        fio[2][j] = "";
+                                        dolzh[j] = "";
+
+                                    }
+                                }
+                            }
+                            i--; 
+                            Console.WriteLine("Досье удалено успешно. Нажмите любую клавишу...");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Досье с таким номером не существует. Нажмите любую клавишу...");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+
+                        break;
+
+
+                    case 4:
+
+
+                        Console.Write("Введите фамилию для поиска: ");
+                        string surname = Console.ReadLine(); // Ввод нужной фамилии
+                        bool isAtLeastOne = false; // Проверка 
+
+
+                        for (int j = 0; j <= i; j++)
+                        {
+
+
+                            if (surname == fio[0][j])
+                            {
+
+                                isAtLeastOne = true;
+                                Console.WriteLine($"{j}. {fio[0][j]} {fio[1][j]} {fio[2][j]} : {dolzh[j]}");
+
+                            }
+
+
+                        }
+                        if (!isAtLeastOne)
+                        {
+
+                            Console.WriteLine("Сотрудников с такой фамилией нет");
+
+
+                        }
+
+                        Console.WriteLine("\n Нажмите любую клавишу...");
+                        Console.ReadKey();
+                        Console.Clear();
+
+                        break;
+
+
+                    case 5:
+
+
+                        isNeeded = false;
+                        break;
+                    default:
+                        Console.WriteLine("Нажмите любую клавишу...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+
+                }
+
+
+            } while (isNeeded);
+        }
+    }
+}
---
+++ SIG_Task_07/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SII_Tusk_07
+{
+    class Program
+    {
+        public static void Shuffle<T>(T[] a)
+        {
+            Random rand = new Random();
+
+            for (int i = a.Length - 1; i >= 1; i--)
+            {
+                int j = rand.Next(i + 1);
+
+                T tmp = a[j];
+                a[j] = a[i];
+                a[i] = tmp;
+            }
+
+        }
+        private static void Main(string[] args)
+        {
+        k:
+            try
+            {
+                Console.WriteLine("Сколько случайных числе будет в массиве?");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] m = new int[n];
+                Random r = new Random();
+                for (int i = 0; i < m.Length; i++)
+                {
+                    m[i] = r.Next(-100, 101);
+                }
+                Console.Write("Ваши числа до изменения:  ");
+                Console.WriteLine(string.Join(" ", m));
+                Shuffle(m);
+                Console.Write("Ваши числа после изменения: ");
+                Console.WriteLine(string.Join(" ", m));
+                Console.ReadKey();
+
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Вы ввели не тот тип данных!" +
+                    "\nПопробуйте еще раз (Программа принимает лишь тип данных в ввиде INT)\n");
+
+                goto k;
+            }
+
+        }
+
+    }
+}
