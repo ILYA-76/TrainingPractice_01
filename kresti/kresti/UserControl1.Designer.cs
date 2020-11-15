54538fff0d06f57bb99c2a30837eb631f630e133
ILYA-76 <74150630+ILYA-76@users.noreply.github.com>
2020-11-15 20:43:13 +0300

    Add files via upload

---
+++ kresti/kresti/UserControl1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AppCSharp
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label1.Location = new System.Drawing.Point(793, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.button1.Location = new System.Drawing.Point(779, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сыграть снова";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label2.Location = new System.Drawing.Point(794, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label3.Location = new System.Drawing.Point(794, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1070, 753);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ kresti/kresti/UserControl1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+namespace AppCSharp
+    {
+        public partial class UserControl1 : UserControl
+        {
+            private int score1 = 0, score2 = 0;
+            private int z = 1;
+            private int x = 12, y = 12;
+            private Button[,] buttons = new Button[3, 3];
+            private int player;
+            public UserControl1()
+            {
+                InitializeComponent();
+                this.Height = 850;
+                this.Width = 1050;
+                player = 1;
+                label1.Text = ($"Текущий ход: Игрок {z}");
+                label2.Text = ($"Игрок 1 побед: {score1}");
+                label3.Text = ($"Игрок 2 побед: {score2}");
+            for (int i = 0; i < buttons.Length / 3; i++)
+                {
+                    for (int j = 0; j < buttons.Length / 3; j++)
+                    {
+                        buttons[i, j] = new Button();
+                        buttons[i, j].Size = new Size(250, 250);
+                    }
+                }
+                setButtons();
+
+            }
+
+            private void setButtons()
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        buttons[i, j].Location = new Point(12 + 248 * j, 12 + 248 * i);
+                        buttons[i, j].Click += button1_Click;
+                        buttons[i, j].Font = new Font(new FontFamily("Microsoft Sans Serif"), 138);
+                        buttons[i, j].Text = "";
+                        this.Controls.Add(buttons[i, j]);
+                    }
+                }
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                switch (player)
+                {
+                    case 1:
+                        sender.GetType().GetProperty("Text").SetValue(sender, "x");
+                        player = 0;
+                        z = 1;
+                        label1.Text = ($"Текущий ход: Игрок 2");
+                        break;
+                    case 0:
+                        sender.GetType().GetProperty("Text").SetValue(sender, "o");
+                        player = 1;
+                        z = 2;
+                        label1.Text = ($"Текущий ход: Игрок 1");
+                        break;
+                }
+                sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+                checkWin();
+            }
+            private void checkWin()
+            {
+
+                if (buttons[0, 0].Text == buttons[0, 1].Text && buttons[0, 1].Text == buttons[0, 2].Text)
+                {
+                    if (buttons[0, 0].Text != "")
+                    {
+                        MessageBox.Show($"Победил игрок {z}");
+                    Scores(); 
+                    return;
+                    }
+                }
+                if (buttons[1, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[1, 2].Text)
+                {
+                    if (buttons[1, 0].Text != "")
+                {
+                    MessageBox.Show($"Победил игрок {z}");
+                    Scores(); 
+                    return;
+                }
+            }
+                if (buttons[2, 0].Text == buttons[2, 1].Text && buttons[2, 1].Text == buttons[2, 2].Text)
+                {
+                    if (buttons[2, 0].Text != "")
+                {
+                    MessageBox.Show($"Победил игрок {z}");
+                    Scores();
+                    return;
+                }
+            }
+                if (buttons[0, 0].Text == buttons[1, 0].Text && buttons[1, 0].Text == buttons[2, 0].Text)
+                {
+                    if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show($"Победил игрок {z}");
+                    Scores(); 
+                    return;
+                }
+            }
+                if (buttons[0, 1].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 1].Text)
+                {
+                    if (buttons[0, 1].Text != "")
+                {
+                    MessageBox.Show($"Победил игрок {z}");
+                    Scores(); 
+                    return;
+                }
+            }
+                if (buttons[0, 2].Text == buttons[1, 2].Text && buttons[1, 2].Text == buttons[2, 2].Text)
+                {
+                    if (buttons[0, 2].Text != "")
+                {
+                    MessageBox.Show($"Победил игрок {z}");
+                    Scores(); 
+                    return;
+                }
+            }
+                if (buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text)
+                {
+                    if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show($"Победил игрок {z}");
+                    Scores(); 
+                    return;
+                }
+            }
+                if (buttons[2, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[0, 2].Text)
+                {
+                    if (buttons[2, 0].Text != "")
+                {
+                    MessageBox.Show($"Победил игрок {z}");
+                    Scores();
+
+                    return;
+                }
+            }
+
+            }
+        public void End(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (buttons[i, j].Text == "")
+                        sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+                }
+            }
+
+
+
+        }
+        private void Scores()
+        {
+            if (z == 1)
+                score1 += 1;
+            if (z == 2)
+                score2 += 1;
+            label2.Text = ($"Игрок 1 побед: {score1}");
+            label3.Text = ($"Игрок 2 побед: {score2}");
+          
+        }
+           
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Text = "";
+                    buttons[i, j].Enabled = true;
+
+                }
+            }
+
+            player = 1;
+            label1.Text = ($"Текущий ход: Игрок 1");
+        }
+       
+
+
+    }
+    }
